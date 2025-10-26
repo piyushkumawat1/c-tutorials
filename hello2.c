d55e0ee361a5f6adf7c1beeda2c2f3ea8fde0420
piyushkumawat1 <piyushkumawat3440@gmail.com>
2025-10-26 12:04:25 +0530

    something

--- hello2.c
+++ hello2.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
-int main(){
-    printf("Hello, World!\n");
-    return 0;
+int main (){
+    printf("Hello , world!");
+    return 0 ;
+    printf("This line is after return statement");
+    //this code show that i am begineer who's has entered in c programming and learning it.
+
 }
