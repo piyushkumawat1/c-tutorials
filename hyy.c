898e079c8a4219d75f844323b7736316be7f988d
piyushkumawat1 <piyushkumawat3440@gmail.com>
2025-11-29 20:06:56 +0530

    update

--- hyy.c
+++ hyy.c
@@ -11,7 +11,7 @@ int main() {
     while (num > 0) {
         rem = num % 10;          // get last digit
         sum = sum + (rem * rem * rem);  // cube and add
-        num = num / 10;          // remove last digit
+        num = num / 10;          // remove last digit that can be that can be done 
     }
 
     if (sum == original)
@@ -20,4 +20,5 @@ int main() {
         printf("%d is not an Armstrong number.", original);
 
     return 0;
+    There have been i don't tell that it can be real for me
 }
